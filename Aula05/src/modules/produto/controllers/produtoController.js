85fc31626ebfc8639bdcb8744a448e9d9843fe7d
LuisF973 <luisfelipe.k.m.rodrigues@gmail.com>
2025-07-17 17:14:17 -0300

    fix: Update jest version and improve error handling in ProdutoController

--- Aula05/src/modules/produto/controllers/produtoController.js
+++ Aula05/src/modules/produto/controllers/produtoController.js
@@ -2,28 +2,34 @@ const produtoModel = require('../models/produtoModel');
 
 class ProdutoController {
 
-    static async criar(req, res) {
-        try {
-            const { nome, preco, estoque } = req.body;
+    // static async criar(req, res) {
+    //     try {
+    //         const { nome, preco, estoque } = req.body;
 
-            if (nome && preco && estoque) {
-                const produto = produtoModel.create({ nome, preco, estoque });
-                res.status(201).json({ mensagem: 'Aluno cadastrado com sucesso', aluno })
-            } else {
-                res.status(400).json({ mensagem: 'Todos os campos devem ser preenchidos' })
-            }
-        } catch (error) {
-            res.status(500).json({ erro: error.message });
-        }
+    //         if (nome && preco && estoque) {
+    //             const produto = produtoModel.create({ nome, preco, estoque });
+    //             res.status(201).json({ mensagem: 'Aluno cadastrado com sucesso', aluno })
+    //         } else {
+    //             res.status(400).json({ mensagem: 'Todos os campos devem ser preenchidos' })
+    //         }
+    //     } catch (error) {
+    //         res.status(500).json({ erro: error.message });
+    //     }
 
 
 
-        return produto
-    }
+    //     return produto
+    // }
     static async criarProduto(dados) {
         const { nome, preco, estoque } = dados.body;
         if (!nome || !preco || !estoque) {
             throw new Error('Todos os campos devem ser preenchidos');
+        }
+        if( typeof nome !== 'string') {
+            throw new Error('Tipo de dado inválido');
+        }
+        if(typeof preco !== 'number' || preco<0){
+            throw new Error('Tipo de dado inválido');
         }
         return await produtoModel.create({ nome, preco, estoque });
     }
--- Aula05/src/modules/produto/tests/produtoController.test.js
+++ Aula05/src/modules/produto/tests/produtoController.test.js
@@ -8,8 +8,8 @@ beforeAll(async () => {
 afterAll(async () => {
     await sequelize.close();
 })
-describe('Testes do Produto Controller', () => {
-    test('Deve criar um produto no banco de dados', async () => {
+describe('Testes do Produto Controller - Criar produto', () => {
+    test('Deve criar um produto corretamente no banco de dados', async () => {
         //Aqui estamos realizando uma requisição para a criação de produto
         const req = { body: { nome: 'Feijão', preco: 4.90, estoque: 500 } }
         const produto = await ProdutoControler.criarProduto(req);
@@ -19,16 +19,37 @@ describe('Testes do Produto Controller', () => {
         expect(produto.preco).toBe(4.90);
     });
 
-    // test('Deve retornar um erro caso não seja passado parametros no body', async () => {
-    //     //Aqui estamos realizando uma requisição para a criação de produto
-    //     const req = { body: {} }
-    //     const produto = await ProdutoControler.criar(req);
-    //     // Analisamos a const produto para verificar os dados retornados
-    //     expect(produto).toHaveProperty('cod_prod');
-    //     expect(produto.nome).toBe('Feijão');
-    //     expect(produto.preco).toBe(4.90);
-    // });
+    test('Deve retornar um erro caso não seja passado parametros no body', async () => {
+        //Aqui estamos realizando uma requisição para a criação de produto
+        const req = { body: {} }
+        // const produto = await ProdutoControler.criarProduto(req);
+        // Analisamos a const produto para verificar os dados retornados
+         await expect(ProdutoControler.criarProduto(req)).rejects.toThrow('Todos os campos devem ser preenchidos');
+         await expect(ProdutoControler.criarProduto(req)).rejects.toThrow('Todos os campos devem ser preenchidos');
+         await expect(ProdutoControler.criarProduto(req)).rejects.toThrow('Todos os campos devem ser preenchidos');
+    });
+    
+    test('Deve retornar um erro caso não seja passado o parametro estoque', async () => {
+        //Aqui estamos realizando uma requisição para a criação de produto
+        const req = { body: { nome:'Feijao',  preco: 4.90 } }
+        // const produto = await ProdutoControler.criarProduto(req);
+        // Analisamos a const produto para verificar os dados retornados
+         await expect(ProdutoControler.criarProduto(req)).rejects.toThrow('Todos os campos devem ser preenchidos');
+    });
 
+    test('Deve retornar um erro caso não seja passado o parametro nome', async () => {
+        //Aqui estamos realizando uma requisição para a criação de produto
+        const req = { body: { nome:'', preco: 4.90,  estoque: 500 } }
+        // const produto = await ProdutoControler.criarProduto(req);
+        // Analisamos a const produto para verificar os dados retornados
+         await expect(ProdutoControler.criarProduto(req)).rejects.toThrow('Todos os campos devem ser preenchidos');
+    });
 
-
+    test('Deve retornar um erro caso o parametro numero seja passado como string', async () => {
+        //Aqui estamos realizando uma requisição para a criação de produto
+        const req = { body: { nome:'Feijão', preco: '4.90', estoque: 500 } }
+        // const produto = await ProdutoControler.criarProduto(req);
+        // Analisamos a const produto para verificar os dados retornados
+         await expect(ProdutoControler.criarProduto(req)).rejects.toThrow('Tipo de dado inválido');
+    });
 })
