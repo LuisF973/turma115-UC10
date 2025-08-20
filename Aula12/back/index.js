d521055cbc2b4a1c4868e9bcbc07aba555754c01
Joel Santos <i3975@rn.senac.local>
2025-08-20 17:15:08 -0300

    feat: Aula 12 inicio do front

--- Aula12/back/index.js
+++ Aula12/back/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { sequelize } = require('./src/config/configDB');
+const cors = require('cors');
 require('dotenv').config();
 const livroRoutes = require('./src/modules/livro/routes/livroRoutes');
 
 const app = express();
 const PORT = process.env.PORT
+app.use(cors())
 app.use(express.json());
 app.use('/livros', livroRoutes);
 
